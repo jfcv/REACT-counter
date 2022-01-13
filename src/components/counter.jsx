f34a6114de72ac38bcf3f0707c67a1ff88c9490f
Juan Cuadros <juan.cuadros@pm.me>
2022-01-13 18:36:00 -0500

    Counter component.

---
+++ src/components/counter.jsx
@@ -0,0 +1,73 @@
+import React, { Component } from "react";
+
+class Counter extends React.Component {
+  state = {
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"],
+  };
+
+  /**
+   * you would have to re-bind every even-handler manually
+   * if using this approach
+   */
+
+  /*
+  constructor() {
+    super();
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+  */
+
+  render() {
+    return (
+      <React.Fragment>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        {this.renderTags()}
+      </React.Fragment>
+    );
+  }
+
+  /**
+   * arrow functions inherit [ this ] keyword
+   */
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  /**
+   * Array.map
+   */
+  renderTags() {
+    return this.state.tags.length === 0 ? (
+      <p>There are no tags!</p>
+    ) : (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+
+  /**
+   * conditional rendering
+   */
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
